9cee4817a72929037b39a99298df06cafed69c0f
nivase-dataterrain <nivase@dataterrain.com>
2024-06-21 22:01:01 +0530

    vercel ignore

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,27 +1,18 @@
-'use client'
-
-/**
- * This configuration is used to for the Sanity Studio that’s mounted on the `\src\app\blog\[[...tool]]\page.tsx` route
- */
-
-import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
-
-// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import {apiVersion, dataset, projectId} from './sanity/env'
-import {schema} from './sanity/schema'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './dobby_blog_sanity/schemaTypes'
 
 export default defineConfig({
-  basePath: '/blog',
-  projectId,
-  dataset,
-  // Add and edit the content schema in the './sanity/schema' folder
-  schema,
-  plugins: [
-    structureTool(),
-    // Vision is a tool that lets you query your content with GROQ in the studio
-    // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: apiVersion}),
-  ],
+  name: 'default',
+  title: 'dobby_blog',
+
+  projectId: '03q548g2',
+  dataset: 'production',
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
 })
